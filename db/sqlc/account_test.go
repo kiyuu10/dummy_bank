d30ee1ca2e54d51b2b8b36eb1024e850bd3f16b5
kiyuu10 <htl.ice14@gmail.com>
2024-04-11 23:51:08 +0700

    Add test for account and add util random

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,9 +1,99 @@
 package db
 
 import (
+	"context"
+	"database/sql"
 	"testing"
+	"time"
+
+	"github.com/kiyuu10/simplebank/util"
+	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
 
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	inputAccount := createRandomAccount(t)
+	expectAccount, err := testQueries.GetAccount(context.Background(), inputAccount.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, expectAccount)
+
+	require.Equal(t, inputAccount.ID, expectAccount.ID)
+	require.Equal(t, inputAccount.Owner, expectAccount.Owner)
+	require.Equal(t, inputAccount.Balance, expectAccount.Balance)
+	require.Equal(t, inputAccount.Currency, expectAccount.Currency)
+	require.WithinDuration(t, inputAccount.CreatedAt, expectAccount.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	inputAccount := createRandomAccount(t)
+
+	updateInfo := UpdateAccountParams{
+		ID:      inputAccount.ID,
+		Balance: util.RandomMoney(),
+	}
+	expectAccount, err := testQueries.UpdateAccount(context.Background(), updateInfo)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, expectAccount)
+
+	require.Equal(t, inputAccount.ID, expectAccount.ID)
+	require.Equal(t, inputAccount.Owner, expectAccount.Owner)
+	require.Equal(t, updateInfo.Balance, expectAccount.Balance)
+	require.Equal(t, inputAccount.Currency, expectAccount.Currency)
+	require.WithinDuration(t, inputAccount.CreatedAt, expectAccount.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	inputAccount := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), inputAccount.ID)
+	require.NoError(t, err)
+
+	expect, err := testQueries.GetAccount(context.Background(), inputAccount.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, expect)
+}
+
+func TestListAccount(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	arg := ListAccountParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
---
+++ util/random.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "qwertyuiopasdfghjklzxcvbnm"
+
+// func init() *rand.Rand {
+// 	source := rand.NewSource(time.Now().UnixNano())
+// 	rng := rand.New(source)
+// 	return rng
+// }
+
+var (
+	source = rand.NewSource(time.Now().UnixNano())
+	rnd    = rand.New(source)
+)
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rnd.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rnd.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// Random Owner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// Random Money generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 10000)
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencis := []string{
+		"EUR", "USD", "CAD",
+	}
+	n := len(currencis)
+	return currencis[rnd.Intn(n)]
+}
